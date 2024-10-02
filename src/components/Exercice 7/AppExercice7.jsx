1abd2cd8c89573f141475dcac20bbb83b770a3e9
MTA <25789885+mtachot@users.noreply.github.com>
2024-10-02 09:29:40 +0100

    Fin exercice 7

--- src/components/Exercice 7/AppExercice7.jsx
+++ src/components/Exercice 7/AppExercice7.jsx
@@ -6,15 +6,15 @@ import { useState } from "react";
 const schema = z.object({
     firstName: z
         .string()
-        .min(3, {message: "Le prénom doit avoir au moins 3 caractères"}),
+        .min(2, {message: "Le prénom doit avoir au moins 2 caractères"}),
     age: z
         .number()
         .min(18, {message: "L'âge doit être au minimum de 18 ans"})    
 });
 
 function AppExercice7 () {
-    const [person, setPerson] = useState(null);
-    const { register, handleSubmit, reset, formState:{errors, isValid}} = useForm({resolver: zodResolver(schema)});
+    const [person, setPerson] = useState({});
+    const { register, handleSubmit, reset, formState:{errors}} = useForm({resolver: zodResolver(schema)});
     const onSubmit = (data) => {
         setPerson(data);
         reset();
@@ -23,20 +23,40 @@ function AppExercice7 () {
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <h1>My form</h1>
-                <label>Age</label>
-                <br/>
-                <input
-                    {...register("age", {valueAsNumber: true})}
-                    id="age"
-                    type="number"
-                    step="any" 
-                    required
-                />
-                <br/><br/>
-                <label>Nom</label>
-                <br/>
-                <input type="text" name="nom"></input>
+                <div className="mb-3">
+                    <h1>My form</h1>
+                    <label htmlFor="age" className="form-label">Âge</label>
+                    <br/>
+                    <input
+                        {...register("age", {valueAsNumber: true})}
+                        id="age"
+                        type="number"
+                        className="form-control"
+                        step="any" 
+                        required
+                    />
+                    {errors.age && (
+                        <p className="text-danger">{errors.age.message}</p>
+                    )}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="firstName" className="form-label">Prénom</label>
+                    <br/>
+                    <input 
+                        {...register("firstName")}
+                        id="firstName"
+                        type="text"
+                        className="form-control"
+                        required
+                    />
+                    {errors.firstName && (
+                        <p className="text-danger">{errors.firstName.message}</p>
+                    )}
+                </div>
+                <button className="btn btn-primary" type="submit">
+                    Valider
+                </button>
+                {person && <div><div>Données envoyées: </div><div>Prénom: {person.firstName} - Âge: {person.age}</div></div>}
             </form>
         </>
     )
